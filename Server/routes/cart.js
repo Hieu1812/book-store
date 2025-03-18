4bd63d542600757bcb2e39d0678fee131146ebc4
Hieu <hieupl2004@gmail.com>
2025-03-18 22:47:33 +0700

    change

--- Server/routes/cart.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartcon = require('../controller/cartcon');
-
-router.post('/addtocart', cartcon.addToCart);
-router.get('/getcart', cartcon.getCart);
-router.delete('/deletecart', cartcon.deleteCart);
-router.post('/changequant', +cartcon.changeQuantity);
-module.exports = router;
