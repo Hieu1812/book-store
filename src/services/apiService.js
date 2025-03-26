8ffa3a8a56a912b7572bc9c1893b602a0e3146b9
Hieu <hieupl2004@gmail.com>
2025-03-26 22:20:51 +0700

    ZALOPAY

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,6 +4,14 @@ const postLogin = (email, password) => {
     return axios.post('https://book-server-sable.vercel.app/login', { email, password });
 };
 
+const postPayment = (user_name, total_price, items) => {
+    return axios.post('https://book-server-sable.vercel.app/payment', { user_name, total_price, items });
+};
+
+const postOrderStatus = (app_trans_id) => {
+    return axios.post(`https://book-server-sable.vercel.app/order-status/${app_trans_id}`, { app_trans_id });
+};
+
 const postRegister = (full_name, email, password, mobile, gender, birthday) => {
     return axios.post('https://book-server-sable.vercel.app/accounts/register', { full_name, email, password, mobile, gender, birthday });
 };
@@ -105,14 +113,17 @@ const getBookReviewbyorderID = (id_order) => {
     return axios.get(`https://book-server-sable.vercel.app/review/getBookReviewbyorderID/?id_order=${id_order}`);
 }
 
-const sendmail = (emailadd,subject, htmlcontent) => {
-    return axios.post(`https://book-server-sable.vercel.app/sendmail`, { emailadd,subject, htmlcontent });
+const sendmail = (emailadd, subject, htmlcontent) => {
+    return axios.post(`https://book-server-sable.vercel.app/sendmail`, { emailadd, subject, htmlcontent });
 }
 const getOrders = () => {
     return axios.get('https://book-server-sable.vercel.app/order/getOrders');
 }
 const getOrderByID = (id_order) => {
     return axios.get(`https://book-server-sable.vercel.app/order/getOrderByID/${id_order}`);
+}
+const getOrderByAccountID = (id_account) => {
+    return axios.get(`https://book-server-sable.vercel.app/order/getOrderByAccountID/${id_account}`);
 }
 const updateOrderStatus = async (id_order, status) => {
     try {
@@ -131,5 +142,6 @@ export {
     getProducts, getProductbyID, getGenre, getInfor, deleteProductbyID,
     getProductbyGenre, postAddress, getAddress, editAddress, deleteAddress,
     editInfor, addOrderAndOrderDetail, addreview, deleteReview, getBookReviewbyID, sendmail,
-    getBookReviewbyorderID, findProduct, changePass, getOrders, updateOrderStatus,getOrderByID
+    getBookReviewbyorderID, findProduct, changePass, getOrders, updateOrderStatus, getOrderByID, getOrderByAccountID,
+    postPayment, postOrderStatus
 };
