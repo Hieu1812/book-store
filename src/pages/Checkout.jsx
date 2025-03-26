ff530496b8fcba71b96056bdf61238fe97631234
Hieu <hieupl2004@gmail.com>
2025-03-27 00:37:17 +0700

    test

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -97,6 +97,7 @@ const Checkout = () => {
         const checkOrderStatus = () => {
           postOrderStatus(app_trans_id).then((response) => {
             const { return_code } = response.data;
+            log.info('Checking order status:', response.data);
 
             switch (return_code) {
               case 1: // Thành công
@@ -110,7 +111,7 @@ const Checkout = () => {
                   navigate('/OrderSuccess', { state: { order: order } });
                 })
                   .catch((error) => {
-                    console.error('Failed to add order details:', error);
+                    log.error('Failed to add order details:', error);
                   });
                 clearInterval(orderCheckInterval);
                 break;
