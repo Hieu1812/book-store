6f94c8a1c83234da9ec14183da27ab233d97e852
Hieu <hieupl2004@gmail.com>
2025-04-10 21:15:52 +0700

    fix

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 
 const postLogin = (email, password) => {
-    return axios.post('https://book-server-sable.vercel.app/login', { email, password });
+    return axios.post('http://localhost:3005/login', { email, password });
 };
 
 const postPayment = (user_name, total_price, items) => {
-    return axios.post('https://book-server-sable.vercel.app/payment', { user_name, total_price, items });
+    return axios.post('http://localhost:3005/payment', { user_name, total_price, items });
 };
 
 const postOrderStatus = (app_trans_id) => {
-    return axios.post(`https://book-server-sable.vercel.app/order-status/${app_trans_id}`, { app_trans_id });
+    return axios.post(`http://localhost:3005/order-status/${app_trans_id}`, { app_trans_id });
 };
 
 const postRegister = (full_name, email, password, mobile, gender, birthday) => {
-    return axios.post('https://book-server-sable.vercel.app/accounts/register', { full_name, email, password, mobile, gender, birthday });
+    return axios.post('http://localhost:3005/accounts/register', { full_name, email, password, mobile, gender, birthday });
 };
 const changePass = (id_account, oldPass, newPass) => {
-    return axios.post('https://book-server-sable.vercel.app/accounts/changepass', id_account, oldPass, newPass)
+    return axios.post('http://localhost:3005/accounts/changepass', id_account, oldPass, newPass)
 }
 const getImage = (idImage) => {
-    return axios.get(`https://book-server-sable.vercel.app/image/${idImage}`);
+    return axios.get(`http://localhost:3005/image/${idImage}`);
 };
 const getGenre = () => {
-    return axios.get('https://book-server-sable.vercel.app/accounts/genre')
+    return axios.get('http://localhost:3005/accounts/genre')
 }
 const postAddProduct = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/products/addproduct', formData, {
+    return axios.post('http://localhost:3005/products/addproduct', formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
@@ -33,7 +33,7 @@ const postAddProduct = (formData) => {
 };
 // co anh
 const postEditProduct = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/products/editproduct', formData, {
+    return axios.post('http://localhost:3005/products/editproduct', formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
@@ -41,93 +41,118 @@ const postEditProduct = (formData) => {
 };
 
 const getProductbyGenre = (id_genre) => {
-    return axios.get(`https://book-server-sable.vercel.app/products/getproductbygenre?id_genre=${id_genre}`);
+    return axios.get(`http://localhost:3005/products/getproductbygenre?id_genre=${id_genre}`);
 };
 
 
 const getProducts = () => {
-    return axios.get('https://book-server-sable.vercel.app/products/getproducts');
+    return axios.get('http://localhost:3005/products/getproducts');
+};
+const getProductsIfExist = () => {
+    return axios.get('http://localhost:3005/products/getProductsIfExist');
 };
 
+const getCountUser = () => {
+    return axios.get('http://localhost:3005/accounts/getCountUser');
+};
+const getTotalOrders = () => {
+    return axios.get('http://localhost:3005/order/getTotalOrders');
+}
+const getTotalSales = () => {
+    return axios.get('http://localhost:3005/order/getTotalSales');
+}
+
+const getPendingOrders = () => {
+    return axios.get('http://localhost:3005/order/getPendingOrders');
+}
+
+const getRecentTransactions = () =>{
+    return axios.get('http://localhost:3005/order/getRecentTransactions');
+}
+
+
 const getProductbyID = (id_book) => {
-    return axios.get(`https://book-server-sable.vercel.app/products/getproductbyid/?id_book=${id_book}`);
+    return axios.get(`http://localhost:3005/products/getproductbyid/?id_book=${id_book}`);
 }
 
 const findProduct = (book_name) => {
-    return axios.get(`https://book-server-sable.vercel.app/products/getproductbyname/?book_name=${book_name}`);
+    return axios.get(`http://localhost:3005/products/getproductbyname/?book_name=${book_name}`);
 }
 
 const getInfor = (id_account) => {
-    return axios.get(`https://book-server-sable.vercel.app/accounts/infor/?id_account=${id_account}`);
+    return axios.get(`http://localhost:3005/accounts/infor/?id_account=${id_account}`);
 }
 
 const editInfor = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/accounts/editinfor', formData, {
+    return axios.post('http://localhost:3005/accounts/editinfor', formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
     });
 };
+const getAccountbyName = (full_name) => {
+    return axios.get(`http://localhost:3005/accounts/getusername/?full_name=${full_name}`);
+}
 const postAddress = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/address/addaddress', formData, {
+    return axios.post('http://localhost:3005/address/addaddress', formData, {
         headers: {
             'Content-Type': 'application/json'
         }
     });
 };
 const editAddress = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/address/editaddress', formData, {
+    return axios.post('http://localhost:3005/address/editaddress', formData, {
         headers: {
             'Content-Type': 'application/json'
         }
     });
 };
 const addOrderAndOrderDetail = (formData) => {
-    return axios.post('https://book-server-sable.vercel.app/order/addOrderAndOrderDetail', formData, {
+    return axios.post('http://localhost:3005/order/addOrderAndOrderDetail', formData, {
         headers: {
             'Content-Type': 'application/json'
         }
     });
 };
 const getAddress = (id_account) => {
-    return axios.get(`https://book-server-sable.vercel.app/address/getaddress/?id_account=${id_account}`);
+    return axios.get(`http://localhost:3005/address/getaddress/?id_account=${id_account}`);
 }
 const deleteProductbyID = (id_book) => {
-    return axios.delete(`https://book-server-sable.vercel.app/products/deleteproduct/?id_book=${id_book}`)
+    return axios.delete(`http://localhost:3005/products/deleteproduct/?id_book=${id_book}`)
 }
 
 const deleteAddress = (address_id) => {
-    return axios.delete(`https://book-server-sable.vercel.app/address/deleteaddress/?address_id=${address_id}`);
+    return axios.delete(`http://localhost:3005/address/deleteaddress/?address_id=${address_id}`);
 }
 
 const addreview = (id_order, rating, review_text, created_at, id_book) => {
-    return axios.post('https://book-server-sable.vercel.app/review/addreview', { id_order, rating, review_text, created_at, id_book });
+    return axios.post('http://localhost:3005/review/addreview', { id_order, rating, review_text, created_at, id_book });
 }
 const deleteReview = (id) => {
-    return axios.delete(`https://book-server-sable.vercel.app/review/deleteReview/?id=${id}`);
+    return axios.delete(`http://localhost:3005/review/deleteReview/?id=${id}`);
 }
 const getBookReviewbyID = (id_book) => {
-    return axios.get(`https://book-server-sable.vercel.app/review/getBookReviewbyID/?id_book=${id_book}`);
+    return axios.get(`http://localhost:3005/review/getBookReviewbyID/?id_book=${id_book}`);
 }
 const getBookReviewbyorderID = (id_order) => {
-    return axios.get(`https://book-server-sable.vercel.app/review/getBookReviewbyorderID/?id_order=${id_order}`);
+    return axios.get(`http://localhost:3005/review/getBookReviewbyorderID/?id_order=${id_order}`);
 }
 
 const sendmail = (emailadd, subject, htmlcontent) => {
-    return axios.post(`https://book-server-sable.vercel.app/sendmail`, { emailadd, subject, htmlcontent });
+    return axios.post(`http://localhost:3005/sendmail`, { emailadd, subject, htmlcontent });
 }
 const getOrders = () => {
-    return axios.get('https://book-server-sable.vercel.app/order/getOrders');
+    return axios.get('http://localhost:3005/order/getOrders');
 }
 const getOrderByID = (id_order) => {
-    return axios.get(`https://book-server-sable.vercel.app/order/getOrderByID/${id_order}`);
+    return axios.get(`http://localhost:3005/order/getOrderByID/${id_order}`);
 }
 const getOrderByAccountID = (id_account) => {
-    return axios.get(`https://book-server-sable.vercel.app/order/getOrderByAccountID/${id_account}`);
+    return axios.get(`http://localhost:3005/order/getOrderByAccountID/${id_account}`);
 }
 const updateOrderStatus = async (id_order, status) => {
     try {
-        const response = await axios.post(`https://book-server-sable.vercel.app/order/${id_order}/status`, {
+        const response = await axios.post(`http://localhost:3005/order/${id_order}/status`, {
             order_status: status.order_status,
             payment_status: status.payment_status,
         });
@@ -143,5 +168,5 @@ export {
     getProductbyGenre, postAddress, getAddress, editAddress, deleteAddress,
     editInfor, addOrderAndOrderDetail, addreview, deleteReview, getBookReviewbyID, sendmail,
     getBookReviewbyorderID, findProduct, changePass, getOrders, updateOrderStatus, getOrderByID, getOrderByAccountID,
-    postPayment, postOrderStatus
+    postPayment, postOrderStatus, getProductsIfExist, getCountUser, getTotalOrders, getTotalSales, getPendingOrders, getAccountbyName, getRecentTransactions
 };
